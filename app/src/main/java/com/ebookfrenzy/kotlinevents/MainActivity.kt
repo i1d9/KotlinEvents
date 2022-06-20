366ca690626d6e2441f961f1ada465da504d691e
i1d9 <42600121+i1d9@users.noreply.github.com>
2022-06-20 19:15:30 +0300

    Added ViewBinding and onClick Event

--- app/src/main/java/com/ebookfrenzy/kotlinevents/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/kotlinevents/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ebookfrenzy.kotlinevents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.ebookfrenzy.kotlinevents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.myButton.setOnClickListener{
+            binding.statusText.text = "Button Clicked"
+        }
     }
 }
